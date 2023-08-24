eebb0e316e53f138ba3a4d7d3984c6f0fa553f32
GabrielAL <65577234+GabrielALDev@users.noreply.github.com>
2023-08-24 20:36:52 -0300

    java-anatomia-classes

--- java-anatomia-classes/src/MinhaClasse.java
+++
@@ -1,5 +0,0 @@
-public class MinhaClasse {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá Turma, Estou iniciando/voltando a ativa na linguagem java");
-    }
-}
